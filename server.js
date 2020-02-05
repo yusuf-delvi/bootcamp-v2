e63f466b2bab5d6e31d6b777af7506ffd9947a8a
yusuf-delvi <yusufmddelvi8@gmail.com>
2020-02-05 16:33:21 +0530

    setup routes, morgan middleware & DB

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhadled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`);
+	// Close server & exit process
+	server.close(() => process.exit(1));
+});
