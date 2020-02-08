32bb4c1cdc7be307a677ed903f0f57b709f76434
yusuf-delvi <yusufmddelvi8@gmail.com>
2020-02-08 12:31:50 +0530

    error handling with express default err handler

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+// eslint-disable-next-line no-unused-vars
 const colors = require('colors');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -25,6 +27,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
