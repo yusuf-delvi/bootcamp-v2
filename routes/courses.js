5f313e1b8410c80699c442e9d41e7f0690e62495
yusuf-delvi <yusufmddelvi8@gmail.com>
2020-02-11 11:03:37 +0530

    update & delete course

--- routes/courses.js
+++ routes/courses.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getCourses, getCourse, addCourse } = require('../controllers/courses');
+const { getCourses, getCourse, addCourse, updateCourse, deleteCourse } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
@@ -8,6 +8,10 @@ router
 	.get(getCourses)
 	.post(addCourse);
 
-router.route('/:id').get(getCourse);
+router
+	.route('/:id')
+	.get(getCourse)
+	.put(updateCourse)
+	.delete(deleteCourse);
 
 module.exports = router;
