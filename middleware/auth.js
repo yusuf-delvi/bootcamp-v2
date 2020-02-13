ef5d8078669640317b57b91588133977bb11780a
yusuf-delvi <yusufmddelvi8@gmail.com>
2020-02-13 14:35:48 +0530

    authorize logged in user via token

---
+++ middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./async');
+const ErrorResponse = require('../utils/errorResponse');
+const User = require('../models/User');
+
+// Protect routes
+exports.protect = asyncHandler(async (req, res, next) => {
+	let token;
+
+	if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+		// eslint-disable-next-line prefer-destructuring
+		token = req.headers.authorization.split(' ')[1];
+	}
+	// else if (req.cookies.token) {
+	// 	({ token } = req.cookies);
+	// }
+
+	// Make sure token exists
+	if (!token) {
+		return next(new ErrorResponse(`Not authorize to access this route`, 401));
+	}
+
+	try {
+		// verify token
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		req.user = await User.findById(decoded.id);
+
+		next();
+	} catch (err) {
+		return next(new ErrorResponse(`Not authorize to access this route`, 401));
+	}
+});
--- routes/courses.js
+++ routes/courses.js
@@ -6,6 +6,8 @@ const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true });
 
+const { protect } = require('../middleware/auth');
+
 router
 	.route('/')
 	.get(
@@ -15,12 +17,12 @@ router
 		}),
 		getCourses
 	)
-	.post(addCourse);
+	.post(protect, addCourse);
 
 router
 	.route('/:id')
 	.get(getCourse)
-	.put(updateCourse)
-	.delete(deleteCourse);
+	.put(protect, updateCourse)
+	.delete(protect, deleteCourse);
 
 module.exports = router;
